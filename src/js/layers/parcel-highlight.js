c18b55482d066722bd7e06bd40ac0d7527a77b7f
Thadd Eldredge <thadd@ese-llc.com>
2026-02-06 18:16:34 -0500

    Update parcel-highlight.js

--- src/js/layers/parcel-highlight.js
+++ src/js/layers/parcel-highlight.js
@@ -1,13 +1,13 @@
 function addParcelHighlightLayer() {
     map.addSource('parcel highlight', {
         type: 'vector',
-        url: 'mapbox://ese-toh.1ya31624'
+        url: 'mapbox://ese-toh.c0yoak91'
     });
     map.addLayer({
         'id': 'parcel highlight',
         'type': 'line',
         'source': 'parcel highlight',
-        'source-layer': 'CapeTowns-2icvsh',
+        'source-layer': 'PARCEL_LINES_SE-96csbl',
         'layout': {
             'visibility': 'none',
             'line-join': 'round',
