e903450e40bb75edbe8fb15f52bc839e3c9102f1
Thadd Eldredge <thadd@ese-llc.com>
2026-02-06 18:21:38 -0500

    Update parcel-highlight.js

--- src/js/layers/parcel-highlight.js
+++ src/js/layers/parcel-highlight.js
@@ -1,4 +1,5 @@
 function addParcelHighlightLayer() {
+	//SE Mass
     map.addSource('parcel highlight', {
         type: 'vector',
         url: 'mapbox://ese-toh.c0yoak91'
@@ -18,6 +19,67 @@ function addParcelHighlightLayer() {
             'line-width': 1.5
         }
     });
+	//NE Mass
+    map.addSource('parcel highlight', {
+        type: 'vector',
+        url: 'mapbox://ese-toh.6fb8one0'
+    });
+    map.addLayer({
+        'id': 'parcel highlight',
+        'type': 'line',
+        'source': 'parcel highlight',
+        'source-layer': 'PARCEL_LINES_NE-30r7f4',
+        'layout': {
+            'visibility': 'none',
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#a0fa39',
+            'line-width': 1.5
+        }
+    });	
+	//CEN Mass
+    map.addSource('parcel highlight', {
+        type: 'vector',
+        url: 'mapbox://ese-toh.4dcv8ivs'
+    });
+    map.addLayer({
+        'id': 'parcel highlight',
+        'type': 'line',
+        'source': 'parcel highlight',
+        'source-layer': 'PARCEL_LINES_CEN-9g69uw',
+        'layout': {
+            'visibility': 'none',
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#a0fa39',
+            'line-width': 1.5
+        }
+    });
+	//WEST Mass
+    map.addSource('parcel highlight', {
+        type: 'vector',
+        url: 'mapbox://ese-toh.bi1n6f4d'
+    });
+    map.addLayer({
+        'id': 'parcel highlight',
+        'type': 'line',
+        'source': 'parcel highlight',
+        'source-layer': 'PARCEL_LINES_WEST-0f2awl',
+        'layout': {
+            'visibility': 'none',
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#a0fa39',
+            'line-width': 1.5
+        }
+    });
+	
 }
 
 addParcelHighlightLayer();
