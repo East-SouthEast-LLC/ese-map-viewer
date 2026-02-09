344a2e45c44297ea0283fb39c938e97a5af7802f
Thadd Eldredge <thadd@ese-llc.com>
2026-02-09 14:51:18 -0500

    Update floodplain.js

--- src/js/layers/floodplain.js
+++ src/js/layers/floodplain.js
@@ -183,6 +183,14 @@ function addFloodplainLayer() {
         }
     });
     
+			map.on('click', 'floodplain-west', function(e) {
+		new mapboxgl.Popup()
+			.setLngLat(e.lngLat)
+			.setHTML("Flood Zone: <strong>{FLD_ZONE}</strong><br>Elevation: <strong>{STATIC_BFE}</strong>")
+			.addTo(map);
+	});
+	
+	
     map.on('mouseenter', 'floodplain-west', function () {
         map.getCanvas().style.cursor = 'pointer';
     });
