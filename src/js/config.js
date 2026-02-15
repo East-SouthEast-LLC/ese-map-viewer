c515e7a5d0496ffd7b0385684c2bc2b021139723
Thadd Eldredge <thadd@ese-llc.com>
2026-02-15 15:09:45 -0500

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 // config.js
 
-        export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZXNlLXRvaCIsImEiOiJjbWVrM2hjMnUwMXVnMnFvYTBnazc2dmtjIn0.UUSb9J5lGL8H7pzEqapwnQ';
+        window.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZXNlLXRvaCIsImEiOiJjbWVrM2hjMnUwMXVnMnFvYTBnazc2dmtjIn0.UUSb9J5lGL8H7pzEqapwnQ';
+		
