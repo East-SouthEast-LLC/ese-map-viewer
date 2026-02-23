8af643c8fe5b9194c66ac836092be2b147f0d2b9
spledeus <thadd@ese-llc.com>
2026-02-23 17:49:21 -0500

    Update counties.js

--- src/js/layers/counties.js
+++ src/js/layers/counties.js
@@ -1,21 +1,29 @@
 function addCountiesLayer() {
-    map.addSource('counties', {
-        type: 'vector',
-        url: 'mapbox://ese-toh.6397vbze'
-    });
-    map.addLayer({
-        'id': 'counties',
-        'type': 'line',
-        'source': 'counties',
-        'source-layer': '2026_COUNTIES-94ibpy',
-        'layout': {
-            'visibility': 'visible'
-        },
-        'paint': {
-            'line-color': '#1152f5',
-            'line-width': 1.5
-        },
-    });
+
+    // add source only if it doesn't exist
+    if (!map.getSource('counties')) {
+        map.addSource('counties', {
+            type: 'vector',
+            url: 'mapbox://ese-toh.6397vbze'
+        });
+    }
+
+    // add layer only if it doesn't exist
+    if (!map.getLayer('counties')) {
+        map.addLayer({
+            'id': 'counties',
+            'type': 'line',
+            'source': 'counties',
+            'source-layer': '2026_COUNTIES-94ibpy',
+            'layout': {
+                'visibility': 'visible'
+            },
+            'paint': {
+                'line-color': '#1152f5',
+                'line-width': 1.5
+            },
+        });
+    }
 }
 
 addCountiesLayer();
